9c78635a71a54fb55a8f1e1a271240d68b6afc67
Matt <147024601+matthew-simms-298@users.noreply.github.com>
2023-12-20 21:28:30 -0700

    init commit

---
+++ portfolio/src/app/components/HeaderComponent.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+export default function HeaderComponent() {
+    return (
+        <header>
+            <div className="bg-red-700 p-20 w-full">
+                <h1 className=" text-4xl">Hello!</h1>
+            </div>
+        </header>
+    )
+}
---
+++ portfolio/src/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center bg-red-950 justify-between p-24">
+    </main>
+  )
+}

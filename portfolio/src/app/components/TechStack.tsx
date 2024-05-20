66a662544434143f080eeb259904947ee5de2ad7
Matt <147024601+matthew-simms-298@users.noreply.github.com>
2024-05-19 23:31:08 -0600

    minor adjustments

--- portfolio/src/app/components/TechStack.tsx
+++ portfolio/src/app/components/TechStack.tsx
@@ -2,11 +2,11 @@
 import Image from "next/image";
 export default function TechStack() {
   return (
-    <div className="flex flex-col items-center mt-20">
+    <div className="flex flex-col items-center mt-20 text-center">
       <h2 className="text-4xl italic font-serif font-thin mb-5">
         Current Tech Stack
       </h2>
-      <div className="flex items-center mt-5 flex-wrap">
+      <div className="flex flex-row items-center justify-center mt-5 flex-wrap">
         <Image
           className="rounded-lg mx-5 hover:scale-150 hover:transition"
           src="/daisyui-logo.jpg"

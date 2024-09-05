6ea96a7096b60e194c6fd7b46cf8ab4b7e816c00
Matt <147024601+matthew-simms-298@users.noreply.github.com>
2024-09-05 13:24:58 -0600

    Updates and what to do
    Employment has been mostly filled in.
    
    Work through skills first then education, there needs to be a proper layout with special categories before we go through with it
    
    Figure out how to use those buttons in the header to skip to different sections within the page. Ad a contact section within the header to make it take you too the footer, that way users can simply get a hub of contacts

--- portfolio/src/app/components/Employment.tsx
+++ portfolio/src/app/components/Employment.tsx
@@ -1,9 +1,40 @@
-'use client'
+"use client";
 
 export default function Employment() {
-    return (
-        <div>
-            <h2 className="text-4xl italic font-serif font-thin mb-5" >Employment and Experience</h2>
+  const TecTOC = [
+    "Spearheaded development of the company website with several pages, while working with other team members in effort of creating a clean and concise application.",
+    "Detailed pages that showcase their case studies as well as research, keeping it user friendly with recognizable and familiar layouts.",
+    "Mobile support for all screen sizes in either landscape or portrait modes. Adaptive to fit to the users needs and preferences.",
+    "Utilizes the vast array of styles to deliver a concise page in effort to enhance readability.",
+    "Custom list control with React's built-in functions for user interactivity.",
+    "Connected with stakeholders to collaborate and create accurate and concise use cases for future development and creation leading up to the final product.",
+  ];
+  return (
+    <div className="py-5 w-1/2">
+      <h2 className="text-4xl italic font-serif font-thin mb-5 text-center">
+        Employment and Experience
+      </h2>
+
+      <div>
+        <div className="flex flex-row justify-between pt-3">
+          <h3 className=" text-lg font-bold">Front-End Web Developer</h3>
+          <h3 className=" text-lg font-bold">January 2024 - present</h3>
         </div>
-    )
+        <p className="italic">
+          <a className="underline" href="https://www.tightoilconsortium.com/" target="_blank">
+            Tight Oil Consortium
+          </a>
+          , within their tech branch known as <a className="underline" href="https://www.tectoc.ca" target="_blank">TecTOC</a>
+        </p>
+
+        <ul className="list-disc ml-10">
+          {TecTOC.map((contents, index) => (
+            <li className="py-1" key={index}>
+              {contents}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }

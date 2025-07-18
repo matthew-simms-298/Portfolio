f5e4f81069b496df6a20ffb30d2156477ac13abf
Matthew Simms <147024601+matthew-simms-298@users.noreply.github.com>
2025-07-17 19:21:12 -0600

    Update Header.tsx

--- portfolio/src/app/components/header-components/Header.tsx
+++ portfolio/src/app/components/header-components/Header.tsx
@@ -7,7 +7,7 @@ import HeaderLinks from "./HeaderLinks"
 
 export default function Header() {
     return (
-        <header id="home" className="text-primary bg-base-200 mb-24 flex flex-col items-center w-full shadow-base-300">
+        <header id="home" className="text-primary bg-base-200 mb-24 flex flex-col items-center w-full shadow-base-300 -z-10">
             <HeaderLinks />
             <AboutMe />
         </header>
